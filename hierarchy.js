ec048b6f6d564bc6f562fcf4f076f66d98095ef9
Tiziana Fanni <tiziana.fanni@diee.unica.it>
2020-03-31 15:30:30 +0200

    doc updated

---
+++ hierarchy.js
@@ -0,0 +1,48 @@
+var hierarchy =
+[
+    [ "it.mdc.tool.core.Acquire", "dc/d29/classit_1_1mdc_1_1tool_1_1core_1_1_acquire.html", null ],
+    [ "it.mdc.tool.core.Activator", "d6/d9e/classit_1_1mdc_1_1tool_1_1core_1_1_activator.html", null ],
+    [ "it.mdc.tool.core.multiDataflowGenerator.ActorManager", "d9/d66/classit_1_1mdc_1_1tool_1_1core_1_1multi_dataflow_generator_1_1_actor_manager.html", null ],
+    [ "it.mdc.tool.powerSaving.CgCellPrinter", "db/d7a/classit_1_1mdc_1_1tool_1_1power_saving_1_1_cg_cell_printer.html", null ],
+    [ "it.mdc.tool.profiling.CombinationsGenerator", "da/de8/classit_1_1mdc_1_1tool_1_1profiling_1_1_combinations_generator.html", null ],
+    [ "it.mdc.tool.core.ConfigManager", "df/d74/classit_1_1mdc_1_1tool_1_1core_1_1_config_manager.html", null ],
+    [ "it.mdc.tool.core.platformComposer.ConfigPrinter", "dd/d4d/classit_1_1mdc_1_1tool_1_1core_1_1platform_composer_1_1_config_printer.html", null ],
+    [ "it.mdc.tool.core.ConfigPrinterCal", "de/d7a/classit_1_1mdc_1_1tool_1_1core_1_1_config_printer_cal.html", null ],
+    [ "it.mdc.tool.powerSaving.CpfPrinter", "d4/d52/classit_1_1mdc_1_1tool_1_1power_saving_1_1_cpf_printer.html", null ],
+    [ "it.mdc.tool.prototyping.DriverPrinter", "d9/d9c/classit_1_1mdc_1_1tool_1_1prototyping_1_1_driver_printer.html", null ],
+    [ "it.mdc.tool.powerSaving.EnGenPrinter", "d1/da6/classit_1_1mdc_1_1tool_1_1power_saving_1_1_en_gen_printer.html", null ],
+    [ "it.mdc.tool.utility.FilesManagerMdc", "df/df0/classit_1_1mdc_1_1tool_1_1utility_1_1_files_manager_mdc.html", null ],
+    [ "it.mdc.tool.core.multiDataflowGenerator.Label", "db/daf/classit_1_1mdc_1_1tool_1_1core_1_1multi_dataflow_generator_1_1_label.html", null ],
+    [ "it.mdc.tool.core.multiDataflowGenerator.Labeler", "db/df2/classit_1_1mdc_1_1tool_1_1core_1_1multi_dataflow_generator_1_1_labeler.html", null ],
+    [ "it.mdc.tool.core.platformComposer.LogicRegionFinder", "d1/d39/classit_1_1mdc_1_1tool_1_1core_1_1platform_composer_1_1_logic_region_finder.html", null ],
+    [ "it.mdc.tool.core.platformComposer.LogicRegionMerger", "dd/da8/classit_1_1mdc_1_1tool_1_1core_1_1platform_composer_1_1_logic_region_merger.html", null ],
+    [ "it.mdc.tool.core.multiDataflowGenerator.Matcher", "d3/d1d/classit_1_1mdc_1_1tool_1_1core_1_1multi_dataflow_generator_1_1_matcher.html", null ],
+    [ "it.mdc.tool.core.MDCBackend", "d6/d7e/classit_1_1mdc_1_1tool_1_1core_1_1_m_d_c_backend.html", null ],
+    [ "it.mdc.tool.core.multiDataflowGenerator.Merger", "d4/d2f/classit_1_1mdc_1_1tool_1_1core_1_1multi_dataflow_generator_1_1_merger.html", [
+      [ "it.mdc.tool.core.multiDataflowGenerator.EmpiricMerger", "d2/d4b/classit_1_1mdc_1_1tool_1_1core_1_1multi_dataflow_generator_1_1_empiric_merger.html", null ],
+      [ "it.mdc.tool.core.multiDataflowGenerator.MoreanoMerger", "d8/d80/classit_1_1mdc_1_1tool_1_1core_1_1multi_dataflow_generator_1_1_moreano_merger.html", null ]
+    ] ],
+    [ "it.mdc.tool.core.NetworkBufferSizeCombiner", "d2/de1/classit_1_1mdc_1_1tool_1_1core_1_1_network_buffer_size_combiner.html", null ],
+    [ "it.mdc.tool.core.NetworkClockDomainCombiner", "db/d8d/classit_1_1mdc_1_1tool_1_1core_1_1_network_clock_domain_combiner.html", null ],
+    [ "it.mdc.tool.utility.NetworkMergerOld", "d5/d58/classit_1_1mdc_1_1tool_1_1utility_1_1_network_merger_old.html", null ],
+    [ "it.mdc.tool.core.platformComposer.NetworkPrinterGeneric", "da/df6/classit_1_1mdc_1_1tool_1_1core_1_1platform_composer_1_1_network_printer_generic.html", null ],
+    [ "it.mdc.tool.utility.PathDefiner", "db/d08/classit_1_1mdc_1_1tool_1_1utility_1_1_path_definer.html", null ],
+    [ "it.mdc.tool.core.platformComposer.PlatformComposer", "d6/d05/classit_1_1mdc_1_1tool_1_1core_1_1platform_composer_1_1_platform_composer.html", [
+      [ "it.mdc.tool.core.platformComposer.NetworkPrinter", "da/d15/classit_1_1mdc_1_1tool_1_1core_1_1platform_composer_1_1_network_printer.html", null ]
+    ] ],
+    [ "it.mdc.tool.powerSaving.PowerController", "d5/dae/classit_1_1mdc_1_1tool_1_1power_saving_1_1_power_controller.html", null ],
+    [ "it.mdc.tool.profiling.Profiler", "d6/d6a/classit_1_1mdc_1_1tool_1_1profiling_1_1_profiler.html", null ],
+    [ "it.mdc.tool.core.platformComposer.ProtocolManager", "df/de8/classit_1_1mdc_1_1tool_1_1core_1_1platform_composer_1_1_protocol_manager.html", null ],
+    [ "it.mdc.tool.core.sboxManagement.SboxActorManager", "dd/d13/classit_1_1mdc_1_1tool_1_1core_1_1sbox_management_1_1_sbox_actor_manager.html", null ],
+    [ "it.mdc.tool.core.sboxManagement.SboxLut", "d3/d22/classit_1_1mdc_1_1tool_1_1core_1_1sbox_management_1_1_sbox_lut.html", null ],
+    [ "it.mdc.tool.core.sboxManagement.SboxLutManager", "dc/d04/classit_1_1mdc_1_1tool_1_1core_1_1sbox_management_1_1_sbox_lut_manager.html", null ],
+    [ "it.mdc.tool.core.SBoxPrinterCal", "d1/d6f/classit_1_1mdc_1_1tool_1_1core_1_1_s_box_printer_cal.html", null ],
+    [ "it.mdc.tool.core.platformComposer.SBoxPrinterGeneric", "d4/de2/classit_1_1mdc_1_1tool_1_1core_1_1platform_composer_1_1_s_box_printer_generic.html", null ],
+    [ "it.mdc.tool.prototyping.ScriptPrinter", "d7/d68/classit_1_1mdc_1_1tool_1_1prototyping_1_1_script_printer.html", null ],
+    [ "it.mdc.tool.core.multiDataflowGenerator.Searcher", "d3/d29/classit_1_1mdc_1_1tool_1_1core_1_1multi_dataflow_generator_1_1_searcher.html", null ],
+    [ "it.mdc.tool.utility.SearcherOld", "d0/dfa/classit_1_1mdc_1_1tool_1_1utility_1_1_searcher_old.html", null ],
+    [ "it.mdc.tool.core.multiDataflowGenerator.Unifier", "d4/daa/classit_1_1mdc_1_1tool_1_1core_1_1multi_dataflow_generator_1_1_unifier.html", null ],
+    [ "it.mdc.tool.prototyping.WrapperPrinter", "db/dfa/classit_1_1mdc_1_1tool_1_1prototyping_1_1_wrapper_printer.html", [
+      [ "it.mdc.tool.prototyping.ArticoPrinter", "d4/da6/classit_1_1mdc_1_1tool_1_1prototyping_1_1_artico_printer.html", null ]
+    ] ]
+];
